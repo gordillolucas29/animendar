28be79764b1b9f51feafe11068b49d31eadeb30e
gordillolucas29 <gordillolucas29@gmail.com>
2022-02-19 07:10:22 -0300

    button dark mode fixed and responsive fixes

--- main.js
+++ main.js
@@ -3,18 +3,20 @@ class Header extends HTMLElement {
         this.innerHTML = `
         <header class="title">
             <h1>Animendar</h1> 
+            <div class="switchDarkMode">
+            <input type="checkbox" class="checkbox" id="checkbox">
+            <label for="checkbox" class="label">
+                <i class="fa-solid fa-cloud-moon" id="moon"></i>
+                <i class="fa-solid fa-cloud-sun" id="sun"></i>
+                <div class="ball"></div>
+            </label>
+            </div>
             <nav class="navigation">
                 <ul>
                         <li><a href="index.html">Home</a></li>     
                         <li><a href="now.html">Now</a></li>
                         <li><a href="us.html">Us</a></li>  
-                        <div class="onoffswitch">
-                            <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" tabindex="0">
-                            <label class="onoffswitch-label" for="myonoffswitch">
-                                <span class="onoffswitch-inner"></span>
-                                <span class="onoffswitch-switch"></span>
-                            </label>
-                        </div>
+
                 </ul>
             </nav>
         </header>
@@ -25,12 +27,15 @@ class Header extends HTMLElement {
 customElements.define('page-header', Header)
 
 
+
 class Footer extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <footer class="footer">
-            <a href="https://github.com/gordillolucas29" target="_blank"><img src="img/github.png" alt="github"></a>
+            <div id="footer">
+            <a href="https://github.com/gordillolucas29" target="_blank"><i class="fa-brands fa-github"></i></a>
             <p>made with a little ♡ (just a little)</p>
+            </div>
         </footer>
         `
     }
@@ -38,31 +43,29 @@ class Footer extends HTMLElement {
 
 customElements.define('page-footer', Footer)
 
+// ---------------------------------------------------------------
 
 
-var myonoffswitch = document.getElementById("myonoffswitch");
+// Toggle Dark Mode
+const checkbox = document.getElementById('checkbox');
 
-myonoffswitch.onclick = function() {
-    document.body.classList.toggle("dark-theme");
+checkbox.addEventListener('change', () => {
+    document.body.classList.toggle('dark');    
 
-    // save in local storage
+    // Save in Local Storage 
 
-    if(document.body.classList.contains('dark-theme')){
+    if (document.body.classList.contains('dark')) {
         localStorage.setItem('dark-mode', 'true');
     } else {
         localStorage.setItem('dark-mode', 'false');
     }
+});
 
-} 
-
-// checkbox
-
-// I will to fix bug with switch button >.<
-
-// obtain values
-
+// Local Storage 
 if(localStorage.getItem('dark-mode') == 'true') {
-    document.body.classList.add("dark-theme");
+    document.body.classList.add("dark");
+    checkbox.checked = true;
 } else {
-    document.body.classList.remove("dark-theme");
+    document.body.classList.remove("dark");
+    checkbox.checked = false;
 }
